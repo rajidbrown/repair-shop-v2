878722c6fee9d3cfd96f9a3c486f1975a2c9f2bd
Raji Brown <126716132+rajidbrown@users.noreply.github.com>
2025-11-06 22:02:51 -0500

    routing error for customer login

--- resources/views/customer/dashboard.blade.php
+++ resources/views/customer/dashboard.blade.php
@@ -13,7 +13,7 @@
 
     <div class="tiles grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-4">
         <a class="tile" href="{{ route('customer.bike') }}">My Bike</a>
-        <a class="tile" href="{{ route('customer.book_appointment.form') }}">Book Appointment</a>
+        <a class="tile" href="{{ route('customer.appointment.form') }}">Book Appointment</a>
         <a class="tile" href="{{ route('customer.appointments') }}">My Appointments</a>
         <a class="tile" href="{{ route('customer.diagnostics') }}">Diagnostics</a>
         <a class="tile" href="{{ route('customer.service_history') }}">Service History</a>
